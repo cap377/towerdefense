4fb76aeac6684794a1aed5a699393e4383031d49
Hayden Westbrook <haydentwestbrook@gmail.com>
2015-04-19 19:39:06 -0700

    Changed image for enemies to an animation

--- src/Enemy.hx
+++ src/Enemy.hx
@@ -12,6 +12,7 @@ class Enemy extends Sprite
 {
 	private var game : Game;
 	private var image : Image;
+	private var animation : MovieClipPlus;
 	public var speed : Float;
 	public var health : Float;
 	public var value : Int;
@@ -22,13 +23,28 @@ class Enemy extends Sprite
 	{
 		super();
 		this.game = game;
-		image = new Image(Root.assets.getTexture(name));
+		//image = new Image(Root.assets.getTexture(name));
+
+		/*
+		Replaced the image with an animation
+		Curently only uses one image, but when more assets are done they can be added to the animation
+		*/
+		//var atlas = Root.assets.getTextureAtlas("assets);  //This line will work once assets have been texture packed
+		var vector = new flash.Vector<starling.textures.Texture>();
+		vector.push(Root.assets.getTexture(name));
+		this.animation = new MovieClipPlus(vector, 8);
+		animation.loop = true;
+		addChild(animation);
+		animation.play();
+		
+
+		this.name = name;
 		this.speed = speed;
 		this.health = health;
 		this.value = value;
 		currentDirection = 2;
 		alive = true;
-		addChild(image);
+		//addChild(image);
 	}
 	
 	//Apply hit damage
