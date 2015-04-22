3b5e52a0714f78d48a6eb3416f87acad3f790340
Hayden Westbrook <haydentwestbrook@gmail.com>
2015-04-21 18:51:11 -0700

    Added bitmap fonts to menus

--- src/Root.hx
+++ src/Root.hx
@@ -99,27 +99,44 @@ class Menu extends Sprite {
 	public function new(root:Root) {
 		super();
 		var background = new Image(Root.assets.getTexture("menu"));
+		addChild(background);
 		
-		var startButton = new Button(Root.assets.getTexture("redmenubutton"));
-		startButton.text = "Play";
+		var startImage = new Image(Root.assets.getTexture("redmenubutton"));
+		startImage.x = 200;
+		startImage.y = 200;
+		addChild(startImage);
+		var startText = new TextField(150, 75, "PLAY", "font", 24, 0xFFFFFF);
+		startText.x = 200;
+		startText.y = 200;
+		addChild(startText);
+		var startButton = new Button(Root.assets.getTexture("button"));
+		startButton.width = 150;
+		startButton.height = 75;
 		startButton.x = 200;
 		startButton.y = 200;
 		startButton.addEventListener(Event.TRIGGERED, function() {
 			root.removeChild(this);
 			root.addChild(new Levels(root));
 		});
+		addChild(startButton);
 
-		var creditsButton = new Button(Root.assets.getTexture("bluemenubutton"));
-		creditsButton.text = "Credits";
+		var creditsImage = new Image(Root.assets.getTexture("bluemenubutton"));
+		creditsImage.x = 200;
+		creditsImage.y = 350;
+		addChild(creditsImage);
+		var creditsText = new TextField(150, 75, "CREDITS", "font", 24, 0xFFFFFF);
+		creditsText.x = 200;
+		creditsText.y = 350;
+		addChild(creditsText);
+		var creditsButton = new Button(Root.assets.getTexture("button"));
+		creditsButton.width = 150;
+		creditsButton.height = 75;
 		creditsButton.x = 200;
 		creditsButton.y = 350;
 		creditsButton.addEventListener(Event.TRIGGERED, function() {
 			root.removeChild(this);
 			root.addChild(new Credits(root));
 		});
-
-		addChild(background);
-		addChild(startButton);
 		addChild(creditsButton);
 
 		root.addChild(this);
@@ -131,14 +148,19 @@ class Credits extends Sprite {
 	public function new(root:Root) {
 		super();
 		var background = new Image(Root.assets.getTexture("menu"));
-		var backButton = new Button(Root.assets.getTexture("redmenubutton"));
-		backButton.text = "Back";
+		addChild(background);
+		
+		var backImage = new Image(Root.assets.getTexture("redmenubutton"));
+		addChild(backImage);
+		var backText = new TextField(150, 75, "BACK", "font", 24, 0xFFFFFF);
+		addChild(backText);
+		var backButton = new Button(Root.assets.getTexture("button"));
+		backButton.width = 150;
+		backButton.height = 75;
 		backButton.addEventListener(Event.TRIGGERED, function() {
 			root.removeChild(this);
 			root.addChild(new Menu(root));
 		});
-
-		addChild(background);
 		addChild(backButton);
 
 		root.addChild(this);
@@ -152,41 +174,64 @@ class Levels extends Sprite {
 		var background = new Image(Root.assets.getTexture("menu"));
 		addChild(background);
 
-		var backButton = new Button(Root.assets.getTexture("redmenubutton"));
-		backButton.text = "Back";
+		var backImage = new Image(Root.assets.getTexture("redmenubutton"));
+		addChild(backImage);
+		var backText = new TextField(150, 75, "BACK", "font", 24, 0xFFFFFF);
+		addChild(backText);
+		var backButton = new Button(Root.assets.getTexture("button"));
+		backButton.width = 150;
+		backButton.height = 75;
 		backButton.addEventListener(Event.TRIGGERED, function() {
 			root.removeChild(this);
 			root.addChild(new Menu(root));
 		});
+		addChild(backButton);
 
 		//Create a button for each level
 		var x = 0;
 		var y = 0;
 		for(i in 0...15) {
-			var levelButton:Button;
 			//Check if level is unlocked
+			var levelImage : Image;
+			var levelText : TextField;
 			if(i <= root.level) {
-				levelButton = new Button(Root.assets.getTexture("redmenubutton"), "Level " + i);
+				levelImage = new Image(Root.assets.getTexture("redmenubutton"));
+				levelText = new TextField(150, 75, "LEVEL " + i, "font", 24, 0xFFFFFF);
+				var levelButton : Button;
+				levelButton = new Button(Root.assets.getTexture("button"));
 				levelButton.addEventListener(Event.TRIGGERED, function() {
 					root.removeChild(this);
 					root.addChild(new LevelPreview(root, i));
 				});
+				levelButton.width = 150;
+				levelButton.height = 75;
+				levelButton.x = 10 + x * (levelImage.width + 5);
+				levelButton.y = 200 + (levelImage.height + 5) * y;
+				levelImage.x = 10 + x * (levelImage.width + 5);
+				levelImage.y = 200 + (levelImage.height + 5) * y;
+				levelText.x = 10 + x * (levelImage.width + 5);
+				levelText.y = 200 + (levelImage.height + 5) * y;
+				addChild(levelImage);
+				addChild(levelText);
+				addChild(levelButton);
 			} else {
-				levelButton = new Button(Root.assets.getTexture("bluemenubutton"), "Level " + i);
+				levelImage = new Image(Root.assets.getTexture("bluemenubutton"));
+				levelText = new TextField(150, 75, "LEVEL " + i, "font", 24, 0xFFFFFF);
+				levelImage.x = 10 + x * (levelImage.width + 5);
+				levelImage.y = 200 + (levelImage.height + 5) * y;
+				levelText.x = 10 + x * (levelImage.width + 5);
+				levelText.y = 200 + (levelImage.height + 5) * y;
+				addChild(levelImage);
+				addChild(levelText);
 			}
-			if (x * levelButton.width > 640 - levelButton.width)
+			if (levelImage.x + levelImage.width + 15 >= 640)
 			{
 				y++;
-				x = 0;
+				x = -1;
 			}
-			levelButton.x = 10 + x * (levelButton.width + 5);
-			levelButton.y = 200 + (levelButton.height + 5) * y;
-			addChild(levelButton);
 			
 			x++;
 		}
-
-		addChild(backButton);
 
 		root.addChild(this);
 	}
@@ -197,24 +242,38 @@ class LevelPreview extends Sprite {
 	public function new(root:Root, level:Int) {
 		super();
 		var background = new Image(Root.assets.getTexture("menu"));
-		var backButton = new Button(Root.assets.getTexture("redmenubutton"));
-		backButton.text = "Back";
+		addChild(background);
+
+		var backImage = new Image(Root.assets.getTexture("redmenubutton"));
+		addChild(backImage);
+		var backText = new TextField(150, 75, "BACK", "font", 24, 0xFFFFFF);
+		addChild(backText);
+		var backButton = new Button(Root.assets.getTexture("button"));
+		backButton.width = 150;
+		backButton.height = 75;
 		backButton.addEventListener(Event.TRIGGERED, function() {
 			root.removeChild(this);
 			root.addChild(new Levels(root));
 		});
+		addChild(backButton);
 
-		var startButton = new Button(Root.assets.getTexture("redmenubutton"));
-		startButton.text = "Play";
+		var startImage = new Image(Root.assets.getTexture("redmenubutton"));
+		startImage.x = 200;
+		startImage.y = 200;
+		addChild(startImage);
+		var startText = new TextField(150, 75, "PLAY", "font", 24, 0xFFFFFF);
+		startText.x = 200;
+		startText.y = 200;
+		addChild(startText);
+		var startButton = new Button(Root.assets.getTexture("button"));
+		startButton.width = 150;
+		startButton.height = 75;
 		startButton.x = 200;
 		startButton.y = 200;
 		startButton.addEventListener(Event.TRIGGERED, function() {
 			root.removeChild(this);
 			root.addChild(new Game(root, level));
 		});
-
-		addChild(background);
-		addChild(backButton);
 		addChild(startButton);
 
 		root.addChild(this);
